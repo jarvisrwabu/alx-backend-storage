1b9c2a18e50ead9461772c6f438e7365fc78c83f
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-17 15:54:30 +0300

    Style should be Glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,9 @@
 -- Use CASE to get the lifespan of band
-SELECT DISTINCT(band_name),
+SELECT band_name,
 CASE
     WHEN split IS NULL THEN 2022 - formed
     ELSE split - formed
 END AS lifespan
 FROM metal_bands
+WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
