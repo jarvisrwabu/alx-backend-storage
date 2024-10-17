17eecf6b815141b4cee8c5f4559dbbbb62da1083
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-17 15:40:27 +0300

    Get lifespan of band using CASE

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- Use CASE to get the lifespan of band
+SELECT band_name,
+CASE
+    WHEN split IS NULL THEN 2022 - formed
+    ELSE split - formed
+END AS lifespan
+FROM metal_bands
+ORDER BY lifespan DESC;
